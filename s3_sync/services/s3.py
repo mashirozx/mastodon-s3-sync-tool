1608f80e48dc2bed99564874195c6ac2b1ec409f
mashirozx <moezhx@outlook.com>
2023-04-06 21:03:27 +0800

    fix: object_key can self update, which leads to invalid retry

--- s3_sync/services/s3.py
+++ s3_sync/services/s3.py
@@ -34,27 +34,36 @@ def sync_file(prefix: str, style: str, id: int, file_name: str, cached: bool = F
     object_key = f"{prefix}/{generate_id_partition(id)}/{style}/{file_name}"
     cached_object_key = f"/cache{object_key}"
 
+    best_computed_object_key = finally_object_key = object_key if not cached else cached_object_key
+
     data = None
 
     try:
         response = s3_source.get_object(
-            Bucket=s3_source_bucket, Key=object_key if not cached else cached_object_key
+            Bucket=s3_source_bucket, Key=finally_object_key
         )
         data = response['Body'].read()
     except Exception as e:
-        logger.info(f"not found {object_key}")
-        object_key = cached_object_key if not cached else object_key
+        logger.info(f"not found {finally_object_key}")
+        print(f"not found {finally_object_key}")
+        finally_object_key = cached_object_key if not cached else object_key
         try:
             response = s3_source.get_object(
-                Bucket=s3_source_bucket, Key=object_key
+                Bucket=s3_source_bucket, Key=finally_object_key
             )
             data = response['Body'].read()
-            logger.info(f"downloaded {object_key}`")
+            logger.info(f"downloaded {finally_object_key}`")
+            print(f"downloaded {finally_object_key}`")
         except Exception as e:
             should_retry_as_png = False
             if get_file_extension(file_name) != "png":
                 new_file_name = replace_file_extension(file_name, "png")
-                logger.info(f"retrying as png {object_key} -> {new_file_name}")
+                logger.info(
+                    f"retrying as png {best_computed_object_key} -> {new_file_name}"
+                )
+                print(
+                    f"retrying as png {best_computed_object_key} -> {new_file_name}"
+                )
                 should_retry_as_png = True
                 is_retry_as_png_succeed, e = sync_file(
                     prefix=prefix,
@@ -64,23 +73,35 @@ def sync_file(prefix: str, style: str, id: int, file_name: str, cached: bool = F
                     cached=cached
                 )
             if (should_retry_as_png and is_retry_as_png_succeed):
-                logger.info(f"retry as png succeed {object_key}")
+                logger.info(
+                    f"retry as png succeed {best_computed_object_key}"
+                )
+                print(
+                    f"retry as png succeed {best_computed_object_key}"
+                )
                 return (True, None)
             elif not should_retry_as_png or (should_retry_as_png and not is_retry_as_png_succeed):
-                logger.error(f"download failed {object_key}", str(e))
+                logger.error(
+                    f"download failed {best_computed_object_key}", str(e)
+                )
+                print(
+                    f"download failed {best_computed_object_key}", str(e)
+                )
                 return (False, e)
 
     try:
         # mime_type = guess_type(data)
         mime_type = from_buffer(data, mime=True) if data else 'empty'
         s3_destination.put_object(
-            Bucket=s3_destination_bucket, Key=object_key, Body=data,
+            Bucket=s3_destination_bucket, Key=finally_object_key, Body=data,
             ContentType=mime_type if mime_type != 'empty' else None,
         )
-        logger.info(f"uploaded {object_key}")
+        logger.info(f"uploaded {finally_object_key}")
+        print(f"uploaded {finally_object_key}")
         return (True, None)
     except Exception as e:
-        logger.error(f"upload failed {object_key}", str(e))
+        logger.error(f"upload failed {finally_object_key}", str(e))
+        print(f"upload failed {finally_object_key}", str(e))
         return (False, e)
 
 
@@ -89,5 +110,6 @@ if __name__ == '__main__':
         prefix="/media_attachments/files",
         style="original",
         id=104530971904218116,
-        file_name="3562dcb3b25b8dd0.jpg"
+        file_name="3562dcb3b25b8dd0.jpg",
+        cached=False
     )
